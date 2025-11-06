cf66dff7c2173aa6d76bda5b4bad4f9c11b47c3f
mohamedagabr <mohamedagabr96@gmai;.com>
2025-11-06 22:34:36 +0200

    Restructure project - move frontend and fix layout

---
+++ backend/src/main/java/com/spring/friendfinder/model/Post.java
@@ -0,0 +1,9 @@
+package com.spring.friendfinder.model;
+
+public class Post {
+    private int postId;
+    private String imagePath;
+    private String postText;
+    private int like;
+    private User user;
+}
---
+++ backend/src/main/java/com/spring/friendfinder/model/User.java
@@ -0,0 +1,29 @@
+package com.spring.friendfinder.model;
+
+import com.spring.friendfinder.enums.Gender;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+import java.util.Date;
+
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private String city;
+    private Date birthDate;
+    private String email;
+
+    private Gender gender ;
+    private List<Post> posts;
+    private List<Friend> friends;
+
+}
